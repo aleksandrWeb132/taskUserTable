065dd613a1acd624682ab2a24f4ec5a2ee2750b0
Александр Тимохин <a.timohin.prog@solo-it.ru>
2024-08-24 16:47:15 +0400

    Добавил тестовые данные для отображения таблицы. Добавил верстку таблицы

--- src/app/client-table/client-table.component.ts
+++ src/app/client-table/client-table.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatTableModule} from "@angular/material/table";
+
+export interface PeriodicElement {
+  checkbox: boolean
+  name: string;
+  surname: string;
+  email: string;
+  phone: string
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: ''},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: ''},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {checkbox: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+];
 
 @Component({
   selector: 'app-client-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './client-table.component.html',
   styleUrls: ['./client-table.component.scss']
 })
-export class ClientTableComponent {
 
+export class ClientTableComponent {
+  displayedColumns: string[] = ['checkbox', 'name', 'surname', 'email', 'phone'];
+  dataSource = ELEMENT_DATA;
 }
