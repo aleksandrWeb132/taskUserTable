51224098b52e31d45f2b5a7b2b584bbdfefd4ccb
Александр Тимохин <a.timohin.prog@solo-it.ru>
2024-08-24 16:05:13 +0400

    Добавил компонент для отображения заголовка

---
+++ src/app/client-header/client-header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-client-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './client-header.component.html',
+  styleUrls: ['./client-header.component.scss']
+})
+export class ClientHeaderComponent {
+
+}
