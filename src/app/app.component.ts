3ccd1e7b6d3391a96da3a87efd7e62c1d49100a9
Александр Тимохин <a.timohin.prog@solo-it.ru>
2024-08-24 16:16:31 +0400

    Подключил и разместил компоненты заголовок и таблица

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {ClientHeaderComponent} from "./client-header/client-header.component";
+import {ClientTableComponent} from "./client-table/client-table.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ClientHeaderComponent, ClientTableComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
