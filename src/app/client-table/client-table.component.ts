000c7cb47bcf0fa1d00bfe66a32b6d8d9a617b2f
Александр Тимохин <a.timohin.prog@solo-it.ru>
2024-08-24 16:05:34 +0400

    Добавил компонент для отображения таблици

---
+++ src/app/client-table/client-table.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-client-table',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './client-table.component.html',
+  styleUrls: ['./client-table.component.scss']
+})
+export class ClientTableComponent {
+
+}
