3850a6cf8580b04b3a1158d3a37d0bd97f8e17ad
Александр Тимохин <a.timohin.prog@solo-it.ru>
2024-08-26 20:18:35 +0400

    Добавил функции для отслеживания выбора элементов таблицы

--- src/app/client-table/client-table.component.ts
+++ src/app/client-table/client-table.component.ts
@@ -1,80 +1,85 @@
-import {Component, computed, signal} from '@angular/core';
+import {Component, computed, signal, ViewChild} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatTableModule} from "@angular/material/table";
-import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatTableDataSource, MatTableModule} from "@angular/material/table";
+import {MatCheckboxChange, MatCheckboxModule} from "@angular/material/checkbox";
 import {FormsModule} from "@angular/forms";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort, MatSortModule} from "@angular/material/sort";
+import {SelectionModel} from "@angular/cdk/collections";
 
 export interface ClientElement {
-  completed: boolean
+  id: number;
   name: string;
   surname: string;
   email: string;
   phone: string;
 }
 
-export interface TableControl {
-  completed: boolean;
-  columns: string[];
-  clients: ClientElement[];
-}
+const CLIENTS: ClientElement[] = [
+  {id: 0, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {id: 1, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {id: 2, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: ''},
+  {id: 3, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {id: 4, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {id: 5, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {id: 6, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {id: 7, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {id: 8, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {id: 9, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {id: 10, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {id: 11, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {id: 12, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: ''},
+  {id: 13, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {id: 14, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {id: 15, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {id: 16, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {id: 17, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
+  {id: 18, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'}
+]
 
 @Component({
   selector: 'app-client-table',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatCheckboxModule, FormsModule],
+  imports: [CommonModule, MatTableModule, MatCheckboxModule, FormsModule, MatSortModule],
   templateUrl: './client-table.component.html',
   styleUrls: ['./client-table.component.scss']
 })
 
 export class ClientTableComponent {
-  readonly tableClient = signal<TableControl>({
-    completed: false,
-    columns: ['checkbox', 'name', 'surname', 'email', 'phone'],
-    clients: [
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: ''},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: ''},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'},
-      {completed: false, name: 'Александр', surname: "Прилучный", email: 'iohl_990@mail.ru', phone: '+79053856195'}
-    ]
-  });
+  displayedColumns: string[] = ['checkbox', 'name', 'surname', 'email', 'phone'];
 
-  readonly partiallyComplete = computed(() => {
-    const task = this.tableClient();
-    if (!task.clients) {
-      return false;
-    }
-    return task.clients.some(t => t.completed) && !task.clients.every(t => t.completed);
-  });
+  dataSource = new MatTableDataSource<ClientElement>(CLIENTS);
+  selection  = new SelectionModel<ClientElement>(true, []);
 
-  update(completed: boolean, index?: number) {
-    this.tableClient.update(task => {
-      if (index === undefined) {
-        task.completed = completed;
-        task.clients?.forEach(t => (t.completed = completed));
-      } else {
-        task.clients![index].completed = completed;
-        task.completed = task.clients?.every(t => t.completed) ?? true;
-      }
-      return {...task};
-    });
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+
+    return numSelected === numRows;
+  }
+  isOneSelected() {
+    return this.selection.selected.length === 0;
   }
 
-  checkLengthClient() {
-    return this.tableClient().clients.length < 1;
+  checkSelectionActive(id: number) {
+    let check = false;
+
+    this.selection.selected.forEach(element => {
+      if(element.id === id) {
+        check = true;
+      }
+    });
+
+    return check;
+  }
+
+  toggleAllRows() {
+    if(this.isAllSelected()) {
+      this.selection.clear();
+
+      return;
+    }
+
+    this.selection.select(...this.dataSource.data);
   }
 }
