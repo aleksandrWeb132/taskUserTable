553fd9a8b468ea7fa1961d6b01e97eacd1d8f490
Александр Тимохин <a.timohin.prog@solo-it.ru>
2024-08-28 14:43:47 +0400

    Реализовал логику компонента для удаления клиентов

--- src/app/client-table/popup-delete-client/popup-delete-client.component.ts
+++ src/app/client-table/popup-delete-client/popup-delete-client.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Client} from "../interface/client";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
 @Component({
   selector: 'app-popup-delete-client',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, MatButtonModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './popup-delete-client.component.html',
   styleUrls: ['./popup-delete-client.component.scss']
 })
 export class PopupDeleteClientComponent {
+  @Input() isVisible = false;
+  @Input() data: Client[] | undefined;
+  @Output() closeDeleteClient = new EventEmitter();
 
+  onClose(flag: number) {
+    let data = this.data;
+
+    if(flag === 0) {
+      data = undefined;
+    }
+
+    this.closeDeleteClient.emit(data);
+  }
 }
